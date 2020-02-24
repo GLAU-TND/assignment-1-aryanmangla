b143ecd79dc731b538a37984a2fb2081b8b4cce1
aryanmangla <61422850+aryanmangla@users.noreply.github.com>
2020-02-24 22:14:17 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -125,7 +125,7 @@ public class Demo {
         Scanner sc = new Scanner(System.in);
         boolean flag = true;
         while (flag == true) {
-            System.out.println("Welcome to AG's Contact List App");
+            System.out.println("Welcome to Aryan's Contact List App");
             System.out.println("Press 1 to add a new contact");
             System.out.println("Press 2 to view all contacts");
             System.out.println("Press 3 to delete a contact");
